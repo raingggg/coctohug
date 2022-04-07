95e51d2d9f0c3349c01af535fe12147b818592f3
raingggg <raingggg@nomail.com>
2022-04-07 20:40:34 +0800

    main path

--- dockers/coctohug-chinilla/chain_launch.sh
+++ dockers/coctohug-chinilla/chain_launch.sh
@@ -16,7 +16,7 @@ mkdir -p /root/.chia/${CHIA_FORK_FOLDER}
 rm -f /root/${CONFIG_PATH}
 ln -s /root/.chia/${CHIA_FORK_FOLDER} /root/${CONFIG_PATH} 
 mkdir -p /root/${CONFIG_PATH}/vanillanet
-ln -s /root/${CONFIG_PATH}/vanillanet /root/${CONFIG_PATH}/mainnet
+mv /root/${CONFIG_PATH}/vanillanet /root/${CONFIG_PATH}/mainnet
 
 mkdir -p /root/${CONFIG_PATH}/mainnet/log
 ${BINARY_NAME} init >> /root/${CONFIG_PATH}/mainnet/log/init.log 2>&1 
